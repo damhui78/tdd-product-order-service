24ad9e93394d431cc8d01b7be0991213de31202a
dam <damhui78@naver.com>
2023-02-19 20:57:06 +0900

    memory to jpa

--- src/main/java/com/dam/tddproductorderservice/product/ProductService.java
+++ src/main/java/com/dam/tddproductorderservice/product/ProductService.java
@@ -1,8 +1,16 @@
 package com.dam.tddproductorderservice.product;
 
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+import javax.transaction.Transactional;
+
+@RestController
+@RequestMapping("/products")
 class ProductService {
     private final ProductPort productPort;
 
@@ -10,9 +18,13 @@ class ProductService {
         this.productPort = productPort;
     }
 
-    public void addProduct(final AddProductRequest request) {
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Void> addProduct(@RequestBody AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
